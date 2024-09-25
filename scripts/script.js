4d0963c5b598cd71efb95b8ccc1061fe7d5724ab
DaphneJager <daphnejager02@gmail.com>
2024-09-25 14:31:48 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,12 @@ var openButton = document.querySelector ("header > button");
 openButton.onclick = openMenu;
 
 
+
 function openMenu() {
   var deNav = document.querySelector("nav");
   deNav.classList.toggle("toonMenu");
 }
 
-
-
-
-
-function myFunction(x) {     
-  x.classList.toggle("change");
- }
 
 
 
