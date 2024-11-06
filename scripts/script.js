ade7ae7f7323f9d129747b3ab03a3d369bc3169a
DaphneJager <daphnejager02@gmail.com>
2024-11-06 16:54:22 +0100

    AHHHH
    doet t alleen bij catalog for some reason

--- scripts/script.js
+++ scripts/script.js
@@ -12,24 +12,23 @@ const countDisplay = document.getElementById("count");
 const sizeButtons = document.querySelectorAll('.size-btn');
 
 
-
 // Functie om het menu te openen en te sluiten
 function openMenu() {
   // Selecteer de nav
-  
+  var deNav = document.querySelector("nav");
   
   // Toggle de class toonMenu op de nav zodat de css de nav kan laten zien of verbergen
   deNav.classList.toggle("toonMenu");
-
   
   // Als de nav wordt getoond, dan wordt de scroll van de body uitgezet
   // Als de nav wordt verborgen, dan wordt de scroll van de body weer aangezet
   if (deNav.classList.contains("toonMenu")) {
     document.body.style.overflow = "hidden";
   } else {
-    document.body.style.overflow = "";
+    document.body.style.overflow = document.body.style.overflow === "hidden" ? "" : "auto";
   }
 }
+
 
 // +-knop
 
